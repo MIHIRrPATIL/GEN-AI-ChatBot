ad841e2f85650d2367082abca52dd20a9abde065
Mihir Patil <mihirpatil2505@gmail.com>
2024-10-01 15:20:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from './Components/Sidebar/sidebar'
+import Main from '../src/Components/Main/Main';
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
